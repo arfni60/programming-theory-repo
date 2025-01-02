1b62987e5ca90f1d06831134d8882b759fe9783d
arfni60 <arfni60@yahoo.com>
2025-01-02 15:48:20 +0100

    score mechanics
    - added scoring
    - added a asset store colorpicker for help
    - save and load high score

---
+++ Assets/Scripts/DataManager.cs
@@ -0,0 +1,61 @@
+using System.IO;
+using UnityEngine;
+
+public class DataManager : MonoBehaviour
+{
+    public static DataManager Instance;
+
+    public int highScore;
+
+    private void Awake()
+    {
+        // start of new code
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        // end of new code
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        LoadHighScore();
+        //Debug.Log("Hello");
+    }
+
+    [System.Serializable]
+    class SaveData
+    {
+        public int highScore;
+    }
+
+    public void SaveHighScore()
+    {
+        SaveData data = new SaveData();
+        data.highScore = highScore;
+
+        string json = JsonUtility.ToJson(data);
+
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+    }
+
+    public void LoadHighScore()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+            highScore = data.highScore;
+        } else
+        {
+            highScore = 0;
+            //Debug.Log(highScore);
+        }
+    }
+
+    
+
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,3 +1,6 @@
+using System.Collections;
+using Unity.Android.Gradle.Manifest;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.Assertions.Must;
 using UnityEngine.UI;
@@ -11,10 +14,27 @@ public class GameManager : MonoBehaviour
     private GameObject displayObject;
 
     private string enteredColor;
+    private string objectColor;
+
+    private int score;
+    private int highScore;
+    public Text scoreText;
+    public Text highScoreText;
+    public GameObject outcomeUI;
+    public Text outcomeText;
+
+    public GameObject colorSwatch;
+    public GameObject exitColorSwatchButton;
+
+    //public GameObject ResetHighScoreButton;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        score = 0;
+        scoreText.text = ("Score: " + score.ToString());
+        //Debug.Log(DataManager.Instance.highScore);
+        highScore = DataManager.Instance.highScore;
+        highScoreText.text = ("High Score: " + highScore.ToString());
         ShowObject(displayObject);
     }
 
@@ -23,11 +43,34 @@ public class GameManager : MonoBehaviour
     {
         if (displayObject != null)
         {
-            Debug.Log(ColorUtility.ToHtmlStringRGB(displayObject.GetComponent<Renderer>().material.GetColor("_Color")));
-            // if (enteredColor != null && displayObject.GetComponent<Renderer>().material.GetColor("_Color") != null)
-            // {
-            //     Debug.Log(displayObject.GetComponent<Renderer>().material.GetColor("_Color"));
-            // }
+            
+            if (enteredColor != null && objectColor != null)
+            {
+                Debug.Log(enteredColor + " " + objectColor);
+                if(enteredColor == objectColor)
+                {
+                    score += 1;
+                    scoreText.text = ("Score: " + score.ToString());
+                    StartCoroutine(ShowOutcomeText("Good guess!"));
+                    //outcomeUI.SetActive(false);
+                    //Debug.Log("Right guess!");
+                    if (DataManager.Instance.highScore == 0 || (score > DataManager.Instance.highScore))
+                    {
+                        DataManager.Instance.highScore = score;
+
+                        highScoreText.text = "High Score : " + DataManager.Instance.highScore;
+                    }
+                    ChangeColor();
+
+                }
+                else
+                {
+                    StartCoroutine(ShowOutcomeText("Wrong. Try again!"));
+                    //outcomeUI.SetActive(false);
+                    Debug.Log("Wrong! Try again!");
+                }
+                enteredColor = null;
+            }
         }
         
     }
@@ -37,6 +80,16 @@ public class GameManager : MonoBehaviour
         displayObject = GameObject.CreatePrimitive(PrimitiveType.Cube);
         displayObject.GetComponent<Renderer>().material = colorMaterial;
         displayObject.transform.position = spawnPosition.transform.position;
+        displayObject.GetComponent<Renderer>().material.SetColor("_BaseColor", Random.ColorHSV(0f, 1f, 0f, 1f, 0f, 1f, 1f, 1f));
+        objectColor = ColorUtility.ToHtmlStringRGB(displayObject.GetComponent<Renderer>().material.GetColor("_BaseColor"));
+        Debug.Log(objectColor);
+    }
+
+    void ChangeColor()
+    {
+        displayObject.GetComponent<Renderer>().material.SetColor("_BaseColor", Random.ColorHSV(0f, 1f, 0f, 1f, 0f, 1f, 1f, 1f));
+        objectColor = ColorUtility.ToHtmlStringRGB(displayObject.GetComponent<Renderer>().material.GetColor("_BaseColor"));
+        Debug.Log(objectColor);
     }
 
     public void ReadColorInput(InputField color)
@@ -44,4 +97,47 @@ public class GameManager : MonoBehaviour
         enteredColor = color.text;
         Debug.Log(enteredColor);
     }
+
+    public void ExitGame()
+    {
+        DataManager.Instance.SaveHighScore();
+        //MainManager.Instance.SaveColor();
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit(); // original code to quit Unity player
+#endif
+
+    }
+
+    IEnumerator ShowOutcomeText(string textToShow)
+    {
+        outcomeUI.SetActive(true);
+        outcomeText.text = textToShow;
+        yield return new WaitForSeconds(2);
+        outcomeUI.SetActive(false);
+
+    }
+
+    public void ResetHighScore()
+    {
+        DataManager.Instance.highScore = 0;
+        DataManager.Instance.SaveHighScore();
+        highScoreText.text = ("High Score: " + DataManager.Instance.highScore);
+
+    }
+
+    public void ShowColorSwatch()
+    {
+        colorSwatch.SetActive(true);
+        exitColorSwatchButton.SetActive(true);
+    }
+
+    public void HideColorSwatch()
+    {
+        colorSwatch.SetActive(false);
+        exitColorSwatchButton.SetActive(false);
+    }
+
+
 }
